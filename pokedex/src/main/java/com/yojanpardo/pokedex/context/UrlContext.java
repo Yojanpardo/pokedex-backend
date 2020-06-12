aa4dc13ca1cf224ab3fe8365e912bd92cb4a10d3
Yojan Pardo <yojan.pardo@gmail.com>
2020-06-12 18:54:56 -0500

    change pokemon api context

--- pokedex/src/main/java/com/yojanpardo/pokedex/context/UrlContext.java
+++ pokedex/src/main/java/com/yojanpardo/pokedex/context/UrlContext.java
@@ -9,7 +9,7 @@ package com.yojanpardo.pokedex.context;
  */
 public class UrlContext {
 	private static final String POKEAPI_CONTEXT = "https://pokeapi.co/api/v2/"; 
-	public static final String POKEMON_CONTEXT = "http://3.128.62.213/8080/api/v1/pokemon";
+	public static final String POKEMON_CONTEXT = "http://3.128.62.213:8080/api/v1/pokemon";
 	// public static final String POKEMON_CONTEXT = "http://localhost:8080/api/v1/pokemon";
 	public static final String POKEMON_SPECIES_URL = POKEAPI_CONTEXT.concat("pokemon-species/");
 	public static final String POKEMON_EVOLUTION_CHAIN_URL = POKEAPI_CONTEXT.concat("evolution-chain/{id}");
